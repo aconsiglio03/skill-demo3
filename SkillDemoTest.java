0e78eefc5a2c970574cc5bc5857761f9024ef35c
aconsiglio03 <97714738+aconsiglio03@users.noreply.github.com>
2022-02-04 18:01:08 -0800

    failing test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiply(){
+        assertEquals(0, SkillDemo.multiply(9, 3));
+    }
+}
